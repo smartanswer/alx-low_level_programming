c32e859a0b70ba085e045ccd7808ac0995eb1af3
smartanswer <calest1995@gmail.com>
2023-03-17 15:41:24 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - The program that print single digit
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
