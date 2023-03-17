e9f4c3aecdd967cd9313ea49cc917b0686aecd91
smartanswer <calest1995@gmail.com>
2023-03-17 00:58:37 +0000

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -The program that prints exactly followed by a new line
+ *
+ * Return: always 1 (success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
