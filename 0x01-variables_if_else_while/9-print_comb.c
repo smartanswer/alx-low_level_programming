b1fdcd5fe56171f954b7d9e6d6293e5bb033deab
smartanswer <calest1995@gmail.com>
2023-03-17 18:55:59 +0000

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - The program that print all possible combination single digit
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i == 9)
+putchar(i + '0');
+else
+{
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
