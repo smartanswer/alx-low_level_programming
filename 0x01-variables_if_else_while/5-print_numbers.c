768b4b690573c4ea4c02ec44c005e8d343fc51ff
smartanswer <calest1995@gmail.com>
2023-03-17 15:32:30 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - The program that print single digit
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d", a);
+printf("\n");
+return (0);
+}
