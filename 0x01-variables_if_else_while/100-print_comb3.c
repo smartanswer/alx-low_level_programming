21ec65599d36c348a9ff3fd0d3e2e9dcec7d705c
smartanswer <calest1995@gmail.com>
2023-03-18 20:34:21 +0000

    combine

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int i, n;
-for (i = 0; i < 10; i++)
+for (i = 0; i < 9; i++)
 {
 for (n = 1; n < 10; n++)
 {
@@ -15,7 +15,7 @@ if (i < n)
 {
 putchar(i + '0');
 putchar(n + '0');
-if (i != 0 || n != 10)
+if (i != 8 || n != 9)
 {
 putchar(',');
 putchar(' ');
@@ -23,6 +23,5 @@ putchar(' ');
 }
 }
 }
-putchar('\n');
 return (0);
 }
