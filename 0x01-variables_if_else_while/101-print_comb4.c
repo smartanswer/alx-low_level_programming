61779b58253a62b97dcef0036c0d08d23fb7fbf2
smartanswer <calest1995@gmail.com>
2023-03-18 21:08:46 +0000

    combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - The program that print all combination
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int i, n, m;
+for (i = 0; i < 8; i++)
+{
+for (n = 1; n < 9; n++)
+{
+for (m = 2; m < 10; m++)
+{
+if (i < n && n < m)
+{
+putchar(i + '0');
+putchar(n + '0');
+putchar(m + '0');
+if (n != 8 || m != 10)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
